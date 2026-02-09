b72d27213a54822fcbbad78039f3812f646ec016
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-10 02:08:57 +0300

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@
 #password_confirmation:virtual string
 class User < ApplicationRecord
     has_secure_password
+    validates :email, presence: true , format: { with: URI::MailTo::EMAIL_REGEXP , message: "must be a valid email address" }
 end
