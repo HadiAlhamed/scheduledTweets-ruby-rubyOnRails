544f2185674e8a4c186a9c1f94445227054795c5
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-10 05:14:35 +0300

    add global current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user # before any action of any controller runs ,
+  # this method will run and set the current user in the Current class
+  # the first thing that happens when a request comes
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id]) # find throws when not found , find_by doesn't throw
+    end
+  end
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
