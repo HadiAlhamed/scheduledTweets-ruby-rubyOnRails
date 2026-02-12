bfe1bd98c4b0f1b4969b7a13eb2c05a58f5b6838
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-12 04:22:56 +0300

    bugfix L omniauth.rb invalid redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,14 +3,18 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.credentials.dig(:mastodon, :client_id),
     Rails.application.credentials.dig(:mastodon, :client_secret),
     {
-      name: :mastodon, # 👈 THIS is what fixes the error
+      name: :mastodon,
       scope: "read:accounts read:statuses write:statuses",
       client_options: {
         site: "https://mastodon.social",
         authorize_url: "/oauth/authorize",
         token_url: "/oauth/token"
-      }
+      },
+      redirect_uri: "http://localhost:3000/auth/mastodon/callback"
+
     }
 end
-
-OmniAuth.config.full_host = "http://localhost:3000"
+OmniAuth.config.full_host = lambda do |env|
+  request = Rack::Request.new(env)
+  "#{request.scheme}://#{request.host_with_port}"
+end
